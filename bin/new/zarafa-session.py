ba5069eeb5a8f238a6d44d999a7af962af2c0ad3
Bob Brandt <bob@brandt.ie>
2016-04-14 15:49:52 +0100

    Work 2016-04-14 15:49:52

--- bin/new/zarafa-session.py
+++ bin/new/zarafa-session.py
@@ -162,10 +162,10 @@ def zarafa_sessions(sessions):
 
 # Start program
 if __name__ == "__main__":
-    command_line_args()
+  command_line_args()
 
-    exitcode = 0
-  # try:
+  exitcode = 0
+  try:
     xmldata = zarafa_sessions(get_data())
 
     if args['output'] == 'xml': 
@@ -173,20 +173,20 @@ if __name__ == "__main__":
       xml.append(xmldata)
       print '<?xml version="1.0" encoding="' + encoding + '"?>\n' + ElementTree.tostring(xml, encoding=encoding, method="xml")
 
-  # except ( Exception, SystemExit ) as err:
-  #   try:
-  #     exitcode = int(err[0])
-  #     errmsg = str(" ".join(err[1:]))
-  #   except:
-  #     exitcode = -1
-  #     errmsg = str(" ".join(err))
+  except ( Exception, SystemExit ) as err:
+    try:
+      exitcode = int(err[0])
+      errmsg = str(" ".join(err[1:]))
+    except:
+      exitcode = -1
+      errmsg = str(" ".join(err))
 
-  #   if args['output'] != 'xml': 
-  #     if exitcode != 0: sys.stderr.write( str(err) +'\n' )
-  #   else:
-  #     xml = ElementTree.Element('zarafaadmin')      
-  #     xmldata = ElementTree.SubElement(xml, 'error', errorcode = str(exitcode) )
-  #     xmldata.text = errmsg
-  #     print '<?xml version="1.0" encoding="' + encoding + '"?>\n' + ElementTree.tostring(xml, encoding=encoding, method="xml")
+    if args['output'] != 'xml': 
+      if exitcode != 0: sys.stderr.write( str(err) +'\n' )
+    else:
+      xml = ElementTree.Element('zarafaadmin')      
+      xmldata = ElementTree.SubElement(xml, 'error', errorcode = str(exitcode) )
+      xmldata.text = errmsg
+      print '<?xml version="1.0" encoding="' + encoding + '"?>\n' + ElementTree.tostring(xml, encoding=encoding, method="xml")
 
-  # sys.exit(exitcode)
+  sys.exit(exitcode)
