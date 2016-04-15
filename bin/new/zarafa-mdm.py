b5666056dc71e8779229bf8f2a73d5c67ee66427
Bob Brandt <bob@brandt.ie>
2016-04-15 12:58:26 +0100

    Work 2016-04-15 12:58:26

--- bin/new/zarafa-mdm.py
+++ bin/new/zarafa-mdm.py
@@ -16,7 +16,9 @@ sys.path.pop()
 args = {}
 args['cache'] = 15
 args['output'] = 'text'
-args['user'] = ''
+args['filter'] = ''
+args['user'] = False
+args['deviceid'] = False
 args['delimiter'] = ""
 
 version = 0.3
@@ -50,7 +52,7 @@ class customUsageVersion(argparse.Action):
       print textwrap.fill(version, self.__row)
       print "\nWritten by Bob Brandt <projects@brandt.ie>."
     else:
-      print "Usage: " + self.__prog + " [options] [username]"
+      print "Usage: " + self.__prog + " [options] [FILTER]"
       print "Script used to find details about Zarafa users.\n"
       print "Options:"
       options = []
@@ -58,8 +60,10 @@ class customUsageVersion(argparse.Action):
       options.append(("-v, --version",           "Show program's version number and exit"))
       options.append(("-o, --output OUTPUT",     "Type of output {text | csv | xml}"))
       options.append(("-c, --cache MINUTES",     "Cache time. (in minutes)"))
-      options.append(("-d, --delimiter DELIM",   "Character to use instead of TAB for field delimiter"))
-      options.append(("username",                "Filter to apply to usernames."))
+      options.append(("    --delimiter DELIM",   "Character to use instead of TAB for field delimiter"))
+      options.append(("-d, --device",            "Apply filter to Device IDs only"))
+      options.append(("-u, --user",              "Apply filter to Usernames only"))
+      options.append(("FILTER",                  "Filter to apply to Usernames or Device IDs."))
       length = max( [ len(option[0]) for option in options ] )
       for option in options:
         description = textwrap.wrap(option[1], (self.__row - length - 5))
@@ -76,7 +80,7 @@ def command_line_args():
           default=args['cache'],
           type=int,
           help="Cache time. (in minutes)")
-  parser.add_argument('-d', '--delimiter',
+  parser.add_argument('--delimiter',
           required=False,
           default=args['delimiter'],
           type=str,
@@ -86,11 +90,21 @@ def command_line_args():
           default=args['output'],
           choices=['text', 'csv', 'xml'],
           help="Display output type.")
-  parser.add_argument('user',
+  parser.add_argument('-d', '--device',
+          required=False,
+          default=args['device'],
+          action='store_true',
+          help="Apply filter to Device IDs only")
+  parser.add_argument('-u', '--user',
+          required=False,
+          default=args['user'],
+          action='store_true',
+          help="Apply filter to Usernames only")            
+  parser.add_argument('filter',
           nargs='?',
-          default= args['user'],
+          default= args['filter'],
           action='store',
-          help="User to retrieve details about.")
+          help="Filter to apply to Usernames or Device IDs.")
   args.update(vars(parser.parse_args()))
   if args['delimiter']: args['delimiter'] = args['delimiter'][0]
   if not args['delimiter'] and args['output'] == "csv": args['delimiter'] = ","
@@ -112,14 +126,17 @@ def get_data():
     out, err = p.communicate()
     if err: raise IOError(err)
 
-    out = out.split('\n')[5:]
+    #out = out.split('\n')[5:]
     tmp =[]
     for c in reversed(range(len(out))):
       if out[c]:
-        deviceID, username = out[c].split(" ",1)
-        username, lastSync = username.strip().split(" ",1)
-        lastSync = lastSync.strip()
-        tmp.append(";".join([deviceID, username, lastSync]))       
+        try:
+          deviceID, username = out[c].split(" ",1)
+          username, lastSync = username.strip().split(" ",1)
+          lastSync = lastSync.strip()
+          tmp.append(";".join([deviceID, username, lastSync]))
+        except:
+          pass     
     out = tmp[:]
 
     f = open(cachefile, 'w')
@@ -176,194 +193,13 @@ def zarafa_devices(devices):
 
   return xml
 
-def zarafa_user(username):
-  global args, ldapmapping
-  command = '/usr/sbin/zarafa-admin --type user --details ' + str(username)
-
-  p = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  out, err = p.communicate()
-  if err: raise IOError(err)
-
-  data = str(out).split("\n")
-  groups = []
-  quotas = []
-  props = []
-  for i in reversed(range(len(data))):
-    if not data[i]: 
-      del data[i]
-    else:
-      if data[i][:8] == "Groups (":
-        groups = data[i:]
-        del data[i:]
-      elif data[i] == "Current user store quota settings:":
-        quotas = data[i:]
-        del data[i:]
-      elif data[i] == "Mapped properties:":
-        props = data[i:]
-        del data[i:]
-
-  del groups[0]
-  groups = [ str(x).lower().strip() for x in groups ]
-  groups.remove("everyone")
-
-  props = [ (str(str(x).split('\t')[1]).lower(), ''.join(str(x).split('\t')[2:])) for x in props[1:] ]
-  props = { x[0]:x[1] for x in props }
-
-  data += quotas[1:]
-  data = [ str(x).replace(":",":\t",1) for x in data ]
-  data = [ ( str(str(x).split('\t')[0]).lower().replace(" ","").replace(":","").replace("-",""), ''.join(str(x).split('\t')[1:]) ) for x in data ]
-  data = { x[0]:x[1] for x in data }
-  data.update(props)
-
-  data["username"] = data.get("username","").lower()
-  data["emailaddress"] = data.get("emailaddress","").lower()
-  if data.has_key("warninglevel"): data["warninglevel"] = data.get("warninglevel","").split(" ")[0]
-  if data.has_key("softlevel"): data["softlevel"] = data.get("softlevel","").split(" ")[0]
-  if data.has_key("hardlevel"): data["hardlevel"] = data.get("hardlevel","").split(" ")[0]
-  if data.has_key("currentstoresize"): data["currentstoresize"] = data.get("currentstoresize","").split(" ")[0]
-  if data.has_key("lastlogon"):
-    data["lastlogon"] = str(datetime.datetime.strptime(data.get("lastlogon").decode('unicode_escape'),'%y/%m/%d %H:%M:%S'))
-  if data.has_key("lastlogoff"):
-    data["lastlogoff"] = str(datetime.datetime.strptime(data.get("lastlogoff").decode('unicode_escape'),'%y/%m/%d %H:%M:%S'))
-
-  for good,bad in ldapmapping:
-    if data.has_key(bad):
-      data[good] = data[bad]
-      del data[bad]
-
-  xml = ElementTree.Element('users')
-  if args['output'] == "text":
-    if not args['delimiter']: args['delimiter'] = "\t"
-    maxlen = max([ len(x) + 4 for x in data.keys() ] + [25])
-    print "Username:".ljust(maxlen), data.get("username","")
-    print "Fullname:".ljust(maxlen), data.get("fullname","")
-    print "Emailaddress:".ljust(maxlen), data.get("emailaddress","")
-    print "Active:".ljust(maxlen), data.get("active","")
-    print "Administrator:".ljust(maxlen), data.get("administrator","")
-    print "Address book:".ljust(maxlen), data.get("addressbook","")
-    print "Auto-accept meeting req:".ljust(maxlen), data.get("autoacceptmeetingreq","")
-    print "Last logon:".ljust(maxlen), data.get("lastlogon","")
-    print "Last logoff:".ljust(maxlen), data.get("lastlogoff","")
-    print "Mapped properties:"
-    print " PR_GIVEN_NAME:".ljust(maxlen), data.get("pr_given_name","")
-    print " PR_INITIALS:".ljust(maxlen), data.get("pr_initials","")
-    print " PR_SURNAME:".ljust(maxlen), data.get("pr_surname","")
-    print " PR_COMPANY_NAME:".ljust(maxlen), data.get("pr_company_name","")
-    print " PR_TITLE:".ljust(maxlen), data.get("pr_title","")
-    print " PR_DEPARTMENT_NAME:".ljust(maxlen), data.get("pr_department_name","")
-    print " PR_OFFICE_LOCATION:".ljust(maxlen), data.get("pr_office_location","")
-    print " PR_BUSINESS_TELEPHONE_NUMBER:".ljust(maxlen), data.get("pr_business_telephone_number","")
-    print " PR_HOME_TELEPHONE_NUMBER:".ljust(maxlen), data.get("pr_home_telephone_number","")      
-    print " PR_PAGER_TELEPHONE_NUMBER:".ljust(maxlen), data.get("pr_pager_telephone_number","")
-    print " PR_PRIMARY_FAX_NUMBER:".ljust(maxlen), data.get("pr_primary_fax_number","")
-    print " PR_BUSINESS_FAX_NUMBER:".ljust(maxlen), data.get("pr_business_fax_number","")
-    print " PR_COUNTRY:".ljust(maxlen), data.get("pr_country","")
-    print " PR_STATE_OR_PROVINCE:".ljust(maxlen), data.get("pr_state_or_province","")
-    print " PR_EMS_AB_IS_MEMBER_OF_DL:".ljust(maxlen), data.get("pr_ems_ab_is_member_of_dl","")
-    print " PR_EC_ENABLED_FEATURES:".ljust(maxlen), data.get("pr_ec_enabled_features","")
-    print " PR_EC_DISABLED_FEATURES:".ljust(maxlen), data.get("pr_ec_disabled_features","")
-    if data.has_key("pr_assistant"): print " PR_ASSISTANT:".ljust(maxlen), data.get("pr_assistant","")
-    if data.has_key("pr_business2_telephone_number"): print " PR_BUSINESS2_TELEPHONE_NUMBER:".ljust(maxlen), data.get("pr_business2_telephone_number","")
-    if data.has_key("pr_business_address_city"): print " PR_BUSINESS_ADDRESS_CITY:".ljust(maxlen), data.get("pr_business_address_city","")
-    if data.has_key("pr_business_home_page"): print " PR_BUSINESS_HOME_PAGE:".ljust(maxlen), data.get("pr_business_home_page","")
-    if data.has_key("pr_childrens_names"): print " PR_CHILDRENS_NAMES:".ljust(maxlen), data.get("pr_childrens_names","")
-    if data.has_key("pr_comment"): print " PR_COMMENT:".ljust(maxlen), data.get("pr_comment","")
-    if data.has_key("pr_company_name"): print " PR_COMPANY_NAME:".ljust(maxlen), data.get("pr_company_name","")
-    if data.has_key("pr_ec_exchange_dn"): print " PR_EC_EXCHANGE_DN:".ljust(maxlen), data.get("pr_ec_exchange_dn","")
-    if data.has_key("pr_ems_ab_owner"): print " PR_EMS_AB_OWNER:".ljust(maxlen), data.get("pr_ems_ab_owner","")
-    if data.has_key("pr_ems_ab_reports"): print " PR_EMS_AB_REPORTS:".ljust(maxlen), data.get("pr_ems_ab_reports","")
-    if data.has_key("pr_ems_ab_www_home_page"): print " PR_EMS_AB_WWW_HOME_PAGE:".ljust(maxlen), data.get("pr_ems_ab_www_home_page","")
-    if data.has_key("pr_home2_telephone_number"): print " PR_HOME2_TELEPHONE_NUMBER:".ljust(maxlen), data.get("pr_home2_telephone_number","")
-    if data.has_key("pr_language"): print " PR_LANGUAGE:".ljust(maxlen), data.get("pr_language","")
-    if data.has_key("pr_manager_name"): print " PR_MANAGER_NAME:".ljust(maxlen), data.get("pr_manager_name","")
-    if data.has_key("pr_mobile_telephone_number"): print " PR_MOBILE_TELEPHONE_NUMBER:".ljust(maxlen), data.get("pr_mobile_telephone_number","")
-    if data.has_key("pr_organizational_id_number"): print " PR_ORGANIZATIONAL_ID_NUMBER:".ljust(maxlen), data.get("pr_organizational_id_number","")
-    if data.has_key("pr_post_office_box"): print " PR_POST_OFFICE_BOX:".ljust(maxlen), data.get("pr_post_office_box","")
-    if data.has_key("pr_postal_address"): print " PR_POSTAL_ADDRESS:".ljust(maxlen), data.get("pr_postal_address","")
-    if data.has_key("pr_postal_code"): print " PR_POSTAL_CODE:".ljust(maxlen), data.get("pr_postal_code","")
-    if data.has_key("pr_street_address"): print " PR_STREET_ADDRESS:".ljust(maxlen), data.get("pr_street_address","")
-    if data.has_key("pr_user_certificate"): print " PR_USER_CERTIFICATE:".ljust(maxlen), data.get("pr_user_certificate","")
-    print "Current user store quota settings:"
-    print " Quota overrides:".ljust(maxlen), data.get("quotaoverrides","")
-    print " Warning level (MB):".ljust(maxlen), data.get("warninglevel","")
-    print " Soft level (MB):".ljust(maxlen), data.get("softlevel","")
-    print " Hard level (MB):".ljust(maxlen), data.get("hardlevel","")
-    print "Current store size (MB):".ljust(maxlen), data.get("currentstoresize","")
-    print "Groups (" + str(len(groups)) + "):"
-    print '-' * (maxlen)
-    print '\n'.join([ " " + str(x) for x in groups ])
-    sys.exit(0)
-
-  elif args['output'] == "csv":
-    tmp = data.get("username","")
-    tmp += ',' + data.get("fullname","")
-    tmp += ',' + data.get("emailaddress","")
-    tmp += ',' + data.get("active","")
-    tmp += ',' + data.get("administrator","")
-    tmp += ',' + data.get("addressbook","")
-    tmp += ',' + data.get("autoacceptmeetingreq","")
-    tmp += ',' + data.get("lastlogon","")
-    tmp += ',' + data.get("lastlogoff","")
-    tmp += ',' + data.get("quotaoverrides","")
-    tmp += ',' + data.get("warninglevel","")
-    tmp += ',' + data.get("softlevel","")
-    tmp += ',' + data.get("hardlevel","")
-    tmp += ',' + data.get("currentstoresize","")
-    tmp += ',' + ';'.join(groups)
-    tmp += ',' + data.get("pr_given_name","")
-    tmp += ',' + data.get("pr_initials","")
-    tmp += ',' + data.get("pr_surname","")
-    tmp += ',' + data.get("pr_company_name","")
-    tmp += ',' + data.get("pr_title","")
-    tmp += ',' + data.get("pr_department_name","")
-    tmp += ',' + data.get("pr_business_telephone_number","")
-    tmp += ',' + data.get("pr_home_telephone_number","")
-    tmp += ',' + data.get("pr_pager_telephone_number","")
-    tmp += ',' + data.get("pr_primary_fax_number","")
-    tmp += ',' + data.get("pr_business_fax_number","")
-    tmp += ',' + data.get("pr_country","")
-    tmp += ',' + data.get("pr_state_or_province","")
-    tmp += ',' + data.get("pr_ems_ab_is_member_of_dl","")
-    tmp += ',' + data.get("pr_ec_enabled_features","")
-    tmp += ',' + data.get("pr_ec_disabled_features","")
-    tmp += ',' + data.get("pr_assistant","")
-    tmp += ',' + data.get("pr_business2_telephone_number","")
-    tmp += ',' + data.get("pr_business_address_city","")
-    tmp += ',' + data.get("pr_business_home_page","")
-    tmp += ',' + data.get("pr_childrens_names","")
-    tmp += ',' + data.get("pr_comment","")
-    tmp += ',' + data.get("pr_company_name","")
-    tmp += ',' + data.get("pr_ec_exchange_dn","")
-    tmp += ',' + data.get("pr_ems_ab_owner","")
-    tmp += ',' + data.get("pr_ems_ab_reports","")
-    tmp += ',' + data.get("pr_ems_ab_www_home_page","")
-    tmp += ',' + data.get("pr_home2_telephone_number","")
-    tmp += ',' + data.get("pr_language","")
-    tmp += ',' + data.get("pr_manager_name","")
-    tmp += ',' + data.get("pr_mobile_telephone_number","")
-    tmp += ',' + data.get("pr_office_location","")
-    tmp += ',' + data.get("pr_organizational_id_number","")
-    tmp += ',' + data.get("pr_post_office_box","")
-    tmp += ',' + data.get("pr_postal_address","")
-    tmp += ',' + data.get("pr_postal_code","")
-    tmp += ',' + data.get("pr_street_address","")
-    tmp += ',' + data.get("pr_user_certificate","")
-    print tmp
-    sys.exit(0)
-
-  else:
-    xmluser = ElementTree.SubElement(xml, 'user', **data)
-    memberof = ElementTree.SubElement(xmluser, 'groups')
-    for group in groups:
-        ElementTree.SubElement(memberof, 'group', groupname = group)
-  return xml
 
 # Start program
 if __name__ == "__main__":
-  command_line_args()
+    command_line_args()
 
-  exitcode = 0
-  try:
+    exitcode = 0
+  # try:
     devices = get_data()
     if len(users) == 1:
       xmldata = zarafa_device(devices[0].split(";")[headers.index("username")])
@@ -376,20 +212,20 @@ if __name__ == "__main__":
       xml.append(xmldata)
       print '<?xml version="1.0" encoding="' + encoding + '"?>\n' + ElementTree.tostring(xml, encoding=encoding, method="xml")
 
-  except ( Exception, SystemExit ) as err:
-    try:
-      exitcode = int(err[0])
-      errmsg = str(" ".join(err[1:]))
-    except:
-      exitcode = -1
-      errmsg = str(" ".join(err))
+  # except ( Exception, SystemExit ) as err:
+  #   try:
+  #     exitcode = int(err[0])
+  #     errmsg = str(" ".join(err[1:]))
+  #   except:
+  #     exitcode = -1
+  #     errmsg = str(" ".join(err))
 
-    if args['output'] != 'xml': 
-      if exitcode != 0: sys.stderr.write( str(err) +'\n' )
-    else:
-      xml = ElementTree.Element('zarafaadmin')      
-      xmldata = ElementTree.SubElement(xml, 'error', errorcode = str(exitcode) )
-      xmldata.text = errmsg
-      print '<?xml version="1.0" encoding="' + encoding + '"?>\n' + ElementTree.tostring(xml, encoding=encoding, method="xml")
+  #   if args['output'] != 'xml': 
+  #     if exitcode != 0: sys.stderr.write( str(err) +'\n' )
+  #   else:
+  #     xml = ElementTree.Element('zarafaadmin')      
+  #     xmldata = ElementTree.SubElement(xml, 'error', errorcode = str(exitcode) )
+  #     xmldata.text = errmsg
+  #     print '<?xml version="1.0" encoding="' + encoding + '"?>\n' + ElementTree.tostring(xml, encoding=encoding, method="xml")
 
-  sys.exit(exitcode)
+  # sys.exit(exitcode)
