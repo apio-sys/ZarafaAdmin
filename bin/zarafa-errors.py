cfc8d398090f42069af1d68c669b8794f4cfc42e
Bob Brandt <bob@brandt.ie>
2016-04-23 15:29:11 +0100

    home 2016-04-23 15:29:11

--- bin/zarafa-errors.py
+++ bin/zarafa-errors.py
@@ -210,8 +210,7 @@ if __name__ == "__main__":
       errmsg = str(" ".join(err[1:]))
     except:
       exitcode = -1
-      print err
-      errmsg = str(" ".join(err))
+      errmsg = str(err)
 
     if args['output'] != 'xml': 
       if exitcode != 0: 
