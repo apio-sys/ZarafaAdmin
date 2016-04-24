896d506ea048326554ab0314a72090373109a1c3
Bob Brandt <bob@brandt.ie>
2016-04-24 22:03:08 +0100

    home 2016-04-24 22:03:08

--- bin/zarafa-groups.py
+++ bin/zarafa-groups.py
@@ -156,9 +156,9 @@ def zarafa_groups(groups):
 
 def zarafa_group(groupname):
   global args, ldapmapping
-  command = ['/usr/sbin/zarafa-admin','--type','group','--details','""' + str(groupname) + '""']
+  command = ['/usr/sbin/zarafa-admin','--type','group','--details',str(groupname)]
 
-  p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
   out, err = p.communicate()
   if err: raise IOError(err)
 
