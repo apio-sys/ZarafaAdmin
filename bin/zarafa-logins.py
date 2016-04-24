eb4136e0e2ea9b114d957e33bec7a95b7a7e69d2
Bob Brandt <bob@brandt.ie>
2016-04-24 21:15:07 +0100

    home 2016-04-24 21:15:07

--- bin/zarafa-logins.py
+++ bin/zarafa-logins.py
@@ -2,10 +2,15 @@
 """
 Python program for Zarafa
 """
-import argparse, sys, datetime, base64
-import xml.etree.ElementTree as ElementTree
+import argparse, textwrap, fnmatch, datetime
+import xml.etree.cElementTree as ElementTree
 import subprocess
 
+# Import Brandt Common Utilities
+import sys, os
+sys.path.append( os.path.realpath( os.path.join( os.path.dirname(__file__), "/opt/brandt/common" ) ) )
+import brandt
+sys.path.pop()
 
 args = {}
 args['output'] = "text"
