af5d930c802f85cadf10958cbd42f400358d1deb
Bob Brandt <bob@brandt.ie>
2016-04-24 21:13:01 +0100

    home 2016-04-24 21:13:01

--- bin/zarafa-logins.py
+++ bin/zarafa-logins.py
@@ -5,7 +5,6 @@ Python program for Zarafa
 import argparse, sys, datetime, base64
 import xml.etree.ElementTree as ElementTree
 import subprocess
-#from multiprocessing import Process, Queue
 
 
 args = {}
@@ -15,25 +14,58 @@ encoding = "utf-8"
 
 months = ('','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec')
 
+class customUsageVersion(argparse.Action):
+  def __init__(self, option_strings, dest, **kwargs):
+    self.__version = str(kwargs.get('version', ''))
+    self.__prog = str(kwargs.get('prog', os.path.basename(__file__)))
+    self.__row = min(int(kwargs.get('max', 80)), brandt.getTerminalSize()[0])
+    self.__exit = int(kwargs.get('exit', 0))
+    super(customUsageVersion, self).__init__(option_strings, dest, nargs=0)
+  def __call__(self, parser, namespace, values, option_string=None):
+    # print('%r %r %r' % (namespace, values, option_string))
+    if self.__version:
+      print self.__prog + " " + self.__version
+      print "Copyright (C) 2013 Free Software Foundation, Inc."
+      print "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
+      version  = "This program is free software: you can redistribute it and/or modify "
+      version += "it under the terms of the GNU General Public License as published by "
+      version += "the Free Software Foundation, either version 3 of the License, or "
+      version += "(at your option) any later version."
+      print textwrap.fill(version, self.__row)
+      version  = "This program is distributed in the hope that it will be useful, "
+      version += "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+      version += "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+      version += "GNU General Public License for more details."
+      print textwrap.fill(version, self.__row)
+      print "\nWritten by Bob Brandt <projects@brandt.ie>."
+    else:
+      print "Usage: " + self.__prog + " [options] [-d|-u] [FILTER]"
+      print "Script used to find details about Zarafa users.\n"
+      print "Options:"
+      options = []
+      options.append(("-h, --help",              "Show this help message and exit"))
+      options.append(("-v, --version",           "Show program's version number and exit"))
+      options.append(("-o, --output OUTPUT",     "Type of output {text | xml}"))
+      length = max( [ len(option[0]) for option in options ] )
+      for option in options:
+        description = textwrap.wrap(option[1], (self.__row - length - 5))
+        print "  " + option[0].ljust(length) + "   " + description[0]
+      for n in range(1,len(description)): print " " * (length + 5) + description[n]
+    exit(self.__exit)
 def command_line_args():
-  global args
-
-  parser = argparse.ArgumentParser(description=".",
-                    formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument('-v', '--version',
-                    action='version',
-                    version="%(prog)s " + str(args['version']) + """
-  Copyright (C) 2011 Free Software Foundation, Inc.
-  License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
-  This is free software: you are free to change and redistribute it.
-  There is NO WARRANTY, to the extent permitted by law.
-  Written by Bob Brandt <projects@brandt.ie>.\n """)
+  global args, version
+  parser = argparse.ArgumentParser(add_help=False)
+  parser.add_argument('-v', '--version', action=customUsageVersion, version=version, max=80)
+  parser.add_argument('-h', '--help', action=customUsageVersion)
   parser.add_argument('-o', '--output',
-                    required=False,
-                    default=args['output'],
-                    choices=['text', 'xml'],
-                    help="Display output type.")
-  args.update(vars(parser.parse_args()))
+                      required=False,
+                      default=args['output'],
+                      choices=['text', 'xml'],
+                      help="Display output type.")
+
+def get_data():
+  global args
+  pass
 
 # Start program
 if __name__ == "__main__":
