3ab260a4116bb19c9a32f941579b9468a1bd998c
Bob Brandt <bob@brandt.ie>
2016-05-01 16:49:01 +0100

    home 2016-05-01 16:49:01

--- bin/zarafa-logins.py
+++ bin/zarafa-logins.py
@@ -216,33 +216,33 @@ def format_users(users):
     for key, label in [ (k, attrsTime[k]['label']) for k in sorted(attrsTime.keys(),key = lambda x: int(attrsTime[x]['min'])) ]:
       tmp = sorted([ u for u in users.keys() if users[u].get(key, 0) > 0 ], key = lambda u: int(users[u][key]), reverse = True)
       if tmp:
-        print str(label).center(usermaxlen + 18)
-        print "Username".ljust(usermaxlen), "  ", "Count"
-        print "-" * (usermaxlen + 9)
+        output = str(label).center(usermaxlen + 18) + "\n"
+        output += "Username".ljust(usermaxlen) + "  Count" + "\n"
+        output += "-" * (usermaxlen + 9) + "\n"
         for u in tmp:
-          print str(u).ljust(usermaxlen), "  ", str(users[u][key]).rjust(5)
-        print
+          output += str(u).ljust(usermaxlen) + "  " + str(users[u][key]).rjust(5) + "\n"
+        print output + "\n"
         
     for user in sorted(users.keys()):
       if users[user].get('samaccountname','') and users[user].get('cn',''):
-        print "User information for " + users[user]['samaccountname'].lower() + " (" + users[user]['cn'] +"):\n" + ("-" * (usermaxlen + 18))
+        output = "User information for " + users[user]['samaccountname'].lower() + " (" + users[user]['cn'] +"):\n" + ("-" * (usermaxlen + 18)) + "\n"
         for key, label in [ (str(k).lower(), attrsLDAP[k]['label']) for k in sorted(attrsLDAP.keys(),key = lambda x: attrsLDAP[x]['sort']) ]:
           if key not in ['cn','samaccountname']:
-            print str(label).rjust(18) + ": " + str(users[user].get(key,""))
-        print
+            output += str(label).rjust(18) + ": " + str(users[user].get(key,"")) + "\n"
+        print output + "\n"
     exitcode = 0
 
   elif args['output'] == "csv":
-    output  = ",".join([ attrsTime[k]['label'] for k in sorted(attrsTime.keys(), key = lambda x: attrsTime[x]['min']) ])
-    output += ","
-    output  = ",".join([ attrsLDAP[k]['label'] for k in sorted(attrsLDAP.keys(), key = lambda x: attrsLDAP[x]['sort']) ])
+    output  = args['delimiter'].join([ attrsTime[k]['label'] for k in sorted(attrsTime.keys(), key = lambda x: attrsTime[x]['min']) ])
+    output += args['delimiter']
+    output  = args['delimiter'].join([ attrsLDAP[k]['label'] for k in sorted(attrsLDAP.keys(), key = lambda x: attrsLDAP[x]['sort']) ])
     print output
 
     attrs = sorted(attrsTime, key = lambda x: attrsTime[x]['min']) + [ a.lower() for a in sorted(attrsLDAP, key = lambda x: attrsLDAP[x]['sort']) ]
     for user in sorted(users.keys()):
-      output = user + ","
+      output = user + args['delimiter']
       for attr in attrs:
-        output += "," + str(users[user].get(attr,""))
+        output += args['delimiter'] + str(users[user].get(attr,""))
       print output
     exitcode = 0
 
