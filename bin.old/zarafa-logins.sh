48b0b2108121abccdf5ba861c4050b32bb786284
Bob Brandt <bob@brandt.ie>
2016-07-13 10:55:54 +0100

    home 2016-07-13 10:55:54

--- bin.old/zarafa-logins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-username="$1"
-ldapsearch -H "ldaps://i.opw.ie/" -LLLx -D "cn=ldapsearch,ou=web,ou=opw,dc=i,dc=opw,dc=ie" -w "D0n't__P@n!c?" "(sAMAccountName=$1)" accountExpires badPasswordTime badPwdCount pwdLastSet lastLogoff lastLogon lastLogonTimestamp logonCount logonHours | perl -p0e "s/\n //g" | sed -n "s|^\S*:|&|p"
