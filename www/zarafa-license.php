70e6a39eaeb747789c7322ecfc74f13baf741570
Bob Brandt <bob@brandt.ie>
2016-07-20 14:28:37 +0100

    Work 2016-07-20 14:28:37

--- www/zarafa-license.php
+++ www/zarafa-license.php
@@ -53,7 +53,7 @@ echo '<div id="loading"><img src="loading.gif"/> Loading...</div>';
 echo str_pad('',$buffer)."\n"; ob_flush();
 
 // XML
-$command = "sudo /opt/brandt/ZarafaAdmin/bin/zarafa-users.py --output xml";
+$command = "sudo /opt/brandt/ZarafaAdmin/bin/zarafa-license.py --output xml";
 $output = shell_exec($command);
 $outputxml = new DOMDocument();
 $outputxml->loadXML( $output );
